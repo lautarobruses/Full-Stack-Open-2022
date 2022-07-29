96e26966160ea07e026355220c68acac35f87d9c
MouGliXx <97257192+MouGliXx@users.noreply.github.com>
2022-07-29 20:18:14 -0300

    4.4-4.5

---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => {
+        return sum + blog.likes
+    }, 0)
+}
+
+module.exports = { dummy, totalLikes }
