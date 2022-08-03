e88223cd3b863facd7b685c6f57a7cc7b492d125
MouGliXx <97257192+MouGliXx@users.noreply.github.com>
2022-08-02 22:07:53 -0300

    4.10-4.14

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -4,23 +4,81 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+describe('GET tests', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('there are two blogs', async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body).toHaveLength(2)
+    })
+
+    test('the name of the unique identifier is "ID"' , async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body[0].id).toBeDefined()
+    })
 })
 
-test('there are two blogs', async () => {
-    const response = await api.get('/api/blogs')
+describe('POST tests', () => {
+    test('a valid blog can be added', async () => {
+        const newBlog = {
+            title: 'blog test',
+            author: 'test author',
+            url: 'https://www.test.com/',
+            likes: 999,
+        }
 
-    expect(response.body).toHaveLength(2)
-})
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
 
-test('the name of the unique identifier is "ID"' , async () => {
-    const response = await api.get('/api/blogs')
+        const response = await api.get('/api/blogs')
 
-    expect(response.body[0].id).toBeDefined()
+        const titles = response.body.map(blog => blog.title)
+
+        expect(response.body).toHaveLength(3)
+        expect(titles).toContain('blog test')
+    })
+
+    test('a blog without likes is posted with the default value', async () => {
+        const newBlog = {
+            title: 'blog test',
+            author: 'test author',
+            url: 'https://www.test.com/',
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await api.get('/api/blogs')
+
+        const likes = response.body.map(blog => blog.likes)
+
+        expect(likes.pop()).toBe(0)
+    })
+
+    test('blog without title is answered with 400 status code', async () => {
+        const newBlog = {
+            author: 'test author',
+            likes: 999,
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+    })
 })
 
 afterAll(() => {
