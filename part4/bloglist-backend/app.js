4273e6ea74539cc02b2abf93dbb318594d7fae24
MouGliXx <97257192+MouGliXx@users.noreply.github.com>
2022-08-06 20:08:54 -0300

    bloglist part 4 change name
    PART 4:
    bloglist -> bloglist-backend

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -21,6 +21,7 @@ mongoose.connect(config.MONGODB_URI)
     })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
